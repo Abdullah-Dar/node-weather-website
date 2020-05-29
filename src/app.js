a57060c8ef2244b97d78fc7286225bfb31111bd8
Abdullah-Dar <darabdullah328@gmail.com>
2020-05-29 16:23:46 +0500

    added a comment

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const forecast=require('./utils/forecast')
 
 const path = require('path')
 const express = require('express')
-
+// ...
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
